cb530343ecbb771b8fb2d2cddb581bcc7da85c42
Senzia Ibrahim <ibrahimsenzia@gmail.com>
2024-03-23 21:22:04 +0300

    strings-lab

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const firstName = currentUser.slice(0, 5);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting =  "Welcome, " + "Grace Hopper".charAt(0) + "!";
+
+
 console.log(firstName); // "Grace"
