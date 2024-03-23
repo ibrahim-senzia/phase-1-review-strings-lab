0896d8b646de038d24c7141fa20fa06a9fa847d3
Senzia Ibrahim <ibrahimsenzia@gmail.com>
2024-03-23 16:08:38 +0300

    string-lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const firstName = currentUser.slice(0, 5);
+console.log(firstName); // "Grace"
